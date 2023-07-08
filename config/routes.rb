93940647cfd639d4f5543ab1469c1b55f5e4be07
Benjamin Wainer <benjawainer@gmail.com>
2023-07-08 21:00:40 +0900

    User can add bookmarks to movies which are shown in each list's page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
   get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %i[index new create show]
+  resources :lists, only: %i[index new create show] do
+    resources :bookmarks, only: %i[new create]
+  end
 end
