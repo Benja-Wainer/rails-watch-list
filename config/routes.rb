3255dba7d9eb13f86c932dca64ddf88f3f247240
Benjamin Wainer <benjawainer@gmail.com>
2023-07-10 13:19:49 +0900

    Added root path and changed path in relevant links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'lists#index'
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: %i[index new create show] do
